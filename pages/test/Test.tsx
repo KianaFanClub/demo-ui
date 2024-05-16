15278fe61b6a093c899e83a634642bf7c2cf2918
lxp <zzxt0019@outlook.com>
2024-05-16 15:05:09 +0800

    初始化项目

---
+++ pages/test/Test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "@/app/globals.css";
+import {Button} from "antd";
+
+const Test: React.FC = () => {
+    return <>
+        <Button className="border-8">123</Button>
+    </>
+}
+export default Test;
