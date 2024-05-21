5165152c424c9af34366c879b84224d663804bbe
lxp <zzxt0019@outlook.com>
2024-05-21 17:16:09 +0800

    测试流水线

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // 引入antd
     transpilePackages: ["antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table"],
+    // 打包
     output: "export",
+    // 打包-路由
+    trailingSlash: true,
 };
 
 export default nextConfig;
