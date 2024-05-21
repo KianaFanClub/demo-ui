16096addf24d1a3abb9e0dfe3d0b31b6cc932087
lxp <zzxt0019@outlook.com>
2024-05-21 17:25:10 +0800

    测试流水线

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    assetPrefix: 'demo-ui',
     // 引入antd
     transpilePackages: ["antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table"],
     // 打包
