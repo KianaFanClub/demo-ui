79310ca666e21118563413ee461f6e9fd2426823
lxp <zzxt0019@outlook.com>
2024-05-22 14:22:20 +0800

    测试流水线

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // 静态文件
-    assetPrefix: '.',
+    basePath: '/demo-ui',
     // 引入antd
     transpilePackages: ["antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table"],
     // 打包
     output: "export",
     // 打包-路由
     trailingSlash: true,
-    // 图片
-    images: {
-        loader: 'custom',
-        loaderFile: './my/image/loader.js',
-    },
 };
 
 export default nextConfig;
